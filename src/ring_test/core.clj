e1ccfc53f753a247d2b9dbfafef40e6b24f9655f
doppioslash <claudia.doppioslash@gmail.com>
2013-03-13 14:31:22 +0000

    Fixed REPL procedure

--- src/ring_test/core.clj
+++ src/ring_test/core.clj
@@ -3,10 +3,12 @@
   (:use ring.util.response)
   (:use ring.middleware.resource)
   (:use ring.middleware.params)
-  (:use ring.middleware.file-info))
+  (:use ring.middleware.file-info)
+  (:use clojure.pprint)
+  (:use ring.middleware.stacktrace))
 
 (defn handler [request]
-  (-> (response "Hello World!")
+  (-> (response "Hello World! :)")
       (content-type "text/plain")))
 
 (defn wrap-content-type [handler content-type]
@@ -14,18 +16,33 @@
     (let [response (handler request)]
       (assoc-in response [:headers "Content-Type"] content-type))))
 
+(defn wrap-spy [handler]
+  (fn [request]
+    (println "------")
+    (println "Incoming request:")
+    (pprint request)
+    (let [response (handler request)]
+      (println "Response map:")
+      (pprint response)
+      (println "-----")
+      response)))
+
 (def app
-  (-> handler
+  (-> #'handler
       (wrap-params)
       (wrap-resource "public")
-      (wrap-file-info)));needs to wrap around wrap-resource or wrap-file
+      (wrap-file-info)
+      (wrap-stacktrace)
+      (wrap-spy)));needs to wrap around wrap-resource or wrap-file
 
 (defonce server (run-jetty #'app {:port 3000 :join? false}))
 
 ;http://stackoverflow.com/questions/2706044/how-do-i-stop-jetty-server-in-clojure
 ;REPL usage
 ;(use 'ring.adapter.jetty)
-;(use 'ring-test.core)
+;Evaluate the functions
 ;(.start server)
 ;(.stop server)
+;re-evaluate changed handler/app
 ;http://mmcgrana.github.com/ring/ring.util.response.html
+;
